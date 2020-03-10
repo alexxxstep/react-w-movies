f787e943119122ec78ccfa5f232c3289bcf1b9f6
alexxxstep <astep2004@gmail.com>
2020-03-10 22:32:07 +0200

    next

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,21 @@ import { moviesData } from "../moviesData";
 import MovieItem from "./MovieItem";
 
 // console.log(moviesData)
+// UI = fn(state, props)
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      movies: moviesData
+      movies: moviesData,
+      moviesWillWatch: []
+
     };
   }
 
   removeMovie = movie => {
-    const updateMovies = this.state.movies.filter(function(item) {
+    const updateMovies = this.state.movies.filter(function (item) {
       return item.id !== movie.id;
     });
     // console.log(updateMovies);
@@ -24,24 +27,42 @@ class App extends Component {
     });
   };
 
+  addMovieToWillWatch = movie => {
+    console.log(movie);
+    // this.state.moviesWillWatch.push(movie)
+    // const updateMoviesWillWatch = [...this.state.moviesWillWatch];
+    // updateMoviesWillWatch.push(movie)
+
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch
+    })
+
+  }
+
   render() {
     console.log("render", this.state, this);
     return (
       <div className="container">
-        <div className="row">
+        <div className="row mt-4">
           <div className="col-9">
-            {this.state.movies.map(movie => {
-              return (
-                <MovieItem
-                  key={movie.id}
-                  movie={movie}
-                  removeMovie={this.removeMovie}
-                />
-              );
-            })}
+            <div className="row">
+              {this.state.movies.map(movie => {
+                return (
+                  <div className="col-6 mb-4" key={movie.id}>
+                    <MovieItem
+                      movie={movie}
+                      removeMovie={this.removeMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                    />
+                  </div>
+                )
+              })}
+            </div>
           </div>
-          <div className='col-3'>
-            <p>Will Watch: 0</p>
+          <div className="col-3">
+            <p>Will Watch: {this.state.moviesWillWatch.length}}</p>
           </div>
         </div>
       </div>
